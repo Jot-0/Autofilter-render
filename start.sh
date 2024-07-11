758a33286debee752f61db00b4a6382ed364afce
Manjot Singh <97024212+Jot4349@users.noreply.github.com>
2024-07-11 21:49:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Jot4349/Autofilter-render/.git /Autofilter-render
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Autofilter-render
-fi
-cd /Autofilter-render
-pip3 install -U -r requirements.txt
+
 echo "Starting Bot...."
 python3 bot.py
