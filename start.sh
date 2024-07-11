73b7eb078176c6b7857539d2ffd6178c41b6a7b0
Manjot Singh <97024212+Jot4349@users.noreply.github.com>
2024-07-11 21:42:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/test1.git /LazyPrincess
+  git clone https://github.com/Jot4349/Autofilter-render/.git /Autofilter-render
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /Autofilter-render
 fi
-cd /LazyPrincess
+cd /Autofilter-render
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
